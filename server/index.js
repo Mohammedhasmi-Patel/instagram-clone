98dd91bb5380b429b008cc7cb16196101f1fad96
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2025-04-17 23:38:28 +0530

    connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,22 +1,25 @@
 import expres, { urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config();
 const app = expres();
+const PORT = process.env.PORT || 3000;
 
 // middleware
 app.use(expres.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
-
-app.use({
-  origin: "http://localhost:5173",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-});
-
-const PORT = 8080;
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`server is listening at ${PORT}`);
 });
